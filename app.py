885d50c045164e8c0ac8f24fc14f33260e803408
Hiroki Kitaoka <hirokky05_0304@yahoo.co.jp>
2020-07-28 21:44:55 +0900

    データベースからの取得ができたやったー

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import sqlite3
 app = Flask(__name__)
 
 
@@ -26,10 +27,25 @@ def index():
     return render_template('index.html', user_name=name, user_address=address, user_age=age)
 
 
-@app.route('/weather')
-def base():
-    weather = "くもり"
-    return render_template('weather.html', dayweather=weather)
+# @app.route('/weather')
+# def base():
+#     weather = "くもり"
+#     return render_template('weather.html', dayweather=weather)
+
+@app.route('/dbtest')
+def dbtest():
+    conn = sqlite3.connect('flasktest.db')
+    c = conn.cursor()
+    # 上の二行はSQLとつなぐときに書くよ
+
+    # SQLの命令を書く
+    c.execute("SELECT name,age,address FROM user WHERE id =1")
+    user_info = c.fetchone()
+
+    # DBの終了
+    c.close()
+    print(user_info)
+    return render_template('dbtest.html', db_userinfo=user_info)
 
 
 if __name__ == '__main__':
