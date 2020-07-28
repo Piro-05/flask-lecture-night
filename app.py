87a41ca5b98af77e20b0eaaf3b4853c0c52ee9ab
Hiroki Kitaoka <hirokky05_0304@yahoo.co.jp>
2020-07-28 20:41:56 +0900

    テンプレートまでやったよ

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -10,6 +10,26 @@ def hello():
 @app.route('/top')
 def top():
     return "ここはトップだよ(^^)/"
+
+
+@app.route('/hello/<text>')
+def namehello(text):
+    return text + "さんこんにちは"
+
+
+@app.route('/index')
+def index():
+    name = "Piro"
+    age = 20
+    address = "円座"
+
+    return render_template('index.html', user_name=name, user_address=address, user_age=age)
+
+
+@app.route('/weather')
+def base():
+    weather = "くもり"
+    return render_template('weather.html', dayweather=weather)
 
 
 if __name__ == '__main__':
